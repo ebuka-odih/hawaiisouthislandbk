febce83fc5c5c247a9e8b1e28c090e0a367a14ad
Gnosis <emmagab38@gmail.com>
2023-09-22 01:52:42 +0100

    first commit

---
+++ resources/views/pages/Contact-Us.blade.php
@@ -0,0 +1,27 @@
+@extends('pages.layout.app2')
+@section('content')
+
+    <main id="main"><table class="Subsection-Header-Content-B-Table scroll-active">
+            <tbody><tr>
+                <td><h1>Contact Us</h1></td>
+            </tr></tbody>
+        </table>
+        <table class="Subsection-Table scroll-active">
+            <tbody><tr>
+                <td><h2 id="anchor-1">Customer Assistance</h2><p>At Nations Star Bank PLC, all of your banking needs can be satisfied by our friendly and professional associates at your local banking center or by reaching out to one of our Customer Service Center representatives.</p><h3>For questions regarding:</h3><ul>
+                        <li>Loan and Deposit Products</li><li>Your Account</li><li>Other Bank Services</li><li>Location Hours and Accessibility</li></ul>
+                    <p style="color: #004b8d">
+{{--                        support@nsbplc.com--}}
+                        <a target="_blank" id="link" href="mailto:support@nsbplc.com?subject=Customer Assistance Website Inquiry">support@nsbplc.com</a>
+                    </p>
+                    <p>Monday – Friday: 7:00 AM – 7:00 PM<br>Saturday: 9:00 AM – 5:00 PM</p><p>Please be advised hours may vary on bank holidays.</p>
+            </tr></tbody>
+        </table>
+
+
+
+    </main>
+    <script src="//code.jivosite.com/widget/4mUuj0iAgO" async></script>
+
+
+@endsection
